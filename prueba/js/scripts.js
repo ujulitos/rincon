9b8d2fdedc2b4f30f1edb06d9132b07b02cf851a
ujulitos <ejs7599@yahoo.com>
2023-04-16 05:16:13 -0600

    16

--- prueba/js/scripts.js
+++ prueba/js/scripts.js
@@ -17,26 +17,26 @@ $win.on('scroll', function() {
         $self.css('opacity', '0');
 
         if ((scrollTop - prev.top) > -600) {
-            $self.waypoint(function() {
-                if ($self.hasClass('wp1')) {
-                    $self.css('opacity', '1').addClass('animated fadeInUp');
-                }
-                if ($self.hasClass('wp2')) {
-                    $self.css('opacity', '1').addClass('animated fadeInLeft');
-                }
-                if ($self.hasClass('wp3')) {
-                    $self.css('opacity', '1').addClass('animated fadeInRight');
-                }
-            }, {
-                offset: '75%'
-            });
+            if ($self.hasClass('wp1')) {
+                $self.css('opacity', '1').addClass('animated fadeInUp');
+            }
+            if ($self.hasClass('wp2')) {
+                $self.css('opacity', '1').addClass('animated fadeInLeft');
+            }
+            if ($self.hasClass('wp3')) {
+                $self.css('opacity', '1').addClass('animated fadeInRight');
+            }
         } else {
-            // console.log('n');
+            // console.log('n', prev);
         }
     });
 
 }).scroll();
 
+
+function abrirLiga(cualLiga) {
+    window.open(cualLiga, '_blank');
+}
 
 function mandarMail() {
     window.open('mailto:elrinconterapeutico013@gmail.com?subject=&body=');
